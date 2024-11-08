3e7ff88f055f2a5c4849be36bacb9e03fe948a05
Abhishek Joshi <abhishekjoshinj@gmail.com>
2024-11-08 12:14:02 -0500

    Generating txt seq files and updating readme

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# COS 551 Final Project
-
-## Pretraining Compute-Optimal Genomic Foundation Models
+# COS 551 Final Project - Pretraining Compute-Optimal Genomic Foundation Models
 
 Team Members: Zirui "Colin" Wang, Abhishek Joshi, Supraj Gunda
 
+## Gerating data
+
+Before generating data, please ensure you have the genomics_benchmarks submodule present in you directory.
+
+Please download NCBI data from [here](https://www.ncbi.nlm.nih.gov/datasets/genome/). Search for the dataset you require and download both the FASTA data and the data corresponding to information about the chromosomes (sequence report). We recommend you download the FASTA data and store it under a directory called `ncbi_data`.
+
+To generate a series of .txt files corresponding to individual sequences of smaller length, run the `scripts/get_sequences.py` file. This takes in three arguments:
+
+- fasta_path: path to the fasta file containing all sequences
+- seq_report_path: path to sequence report path downloaded from the NCBI website corresponding to your FASTA data
+- benchmark_dataset: name of benchmark dataset from `genomic_benchmarks/datasets`
+
+An example command is shown below:
+```
+python scripts/get_sequences.py --fasta_path ncbi_data/ncbi_dataset_mouse/ncbi_dataset/data/GCF_000001635.27/GCF_000001635.27_GRCm39_genomic.fna --seq_report_path ncbi_data/ncbi_dataset_mouse/sequence_report.tsv --benchmark_dataset dummy_mouse_enhancers_ensembl
+```
--- scripts/get_sequences.py
+++ scripts/get_sequences.py
@@ -1,9 +1,39 @@
+import os
 import tqdm
+import gzip
 import pprint
 import argparse
 import pandas as pd
 
 from Bio import SeqIO
+
+def generate_txt_seqs(
+    chromosome_seqs: dict,
+    fasta_path: str,
+    benchmark_dataset: str
+):
+    """
+    Generates a series of txt files where each file corresponds 
+    to a sequence
+    """
+    split = ["train", "test"]
+    classes = ["positive", "negative"]
+
+    for s in split:
+        for c in classes:
+            benchmark_path = os.path.join("genomic_benchmarks/datasets", benchmark_dataset, s, f"{c}.csv.gz")
+            with gzip.open(benchmark_path, "r") as data:
+                df = pd.read_csv(data, sep=',')
+                output_dir = os.path.join(os.path.dirname(fasta_path), "generated_sequences", s, c)
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+                for i, row in tqdm.tqdm(df.iterrows()):
+                    region = row["region"]
+                    seq = str(chromosome_seqs[region][row["start"]:row["end"]]).upper()
+                    output_file = os.path.join(output_dir, f"{i}.txt")
+                    with open(output_file, "w") as f:
+                        f.write(seq)
+            print(f"Completed generating txt seqs for {s}, {c}")
 
 if __name__ == "__main__":
 
@@ -20,20 +50,37 @@ if __name__ == "__main__":
         required=True,
         help="path to .tsv sequence report (information about chromosome to id mapping)"
     )
+    parser.add_argument(
+        "--benchmark_dataset",
+        type=str,
+        required=True,
+        help="genomic benchmarks dataset (i.e. dummy_mouse_enhancers_ensembl)"
+    )
     args = parser.parse_args()
 
+    # creates mapping from refseq id and chromosome number
     df = pd.read_csv(args.seq_report_path, sep='\t')
     id_name_mapping = {}
-    for index, row in df.iterrows():
+    for _, row in df.iterrows():
         if row["Molecule type"] == "Chromosome" and row["RefSeq seq accession"].startswith("NC_"):
             id_name_mapping[row["RefSeq seq accession"]] = row["UCSC style name"]
 
+    # parses the fasta file and gets the sequences
     chromosome_seqs = {}
     for record in tqdm.tqdm(SeqIO.parse(args.fasta_path, "fasta")):
         recid = record.id
         recseq = record.seq
         if recid in id_name_mapping:
             chromosome_seqs[id_name_mapping[recid]] = recseq
+
+    generate_txt_seqs(chromosome_seqs=chromosome_seqs,
+                      fasta_path=args.fasta_path,
+                      benchmark_dataset=args.benchmark_dataset)
+
+    
+
+
+
 
     
         
